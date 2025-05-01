45b38507eed8817318b8539fead3199f0e1f6a98
alessandroschiano98 <schianoski98@gmail.com>
2025-05-01 15:05:34 +0200

    inizio esercizio

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,22 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
-    
+
+    // VARIABILI DI ISTANZA, PROPRIETA' ECC
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public int prezzo;
+    public int iva;
+
+    public Prodotto(int codice, String nome, String descrizione, int prezzo, int iva) {
+        this.codice = codice;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+    // METODI
+
+
 }
